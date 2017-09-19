cdaae5e85cb4073c31d870d973948527e077d378
Krzysztof Wegiel <hello@wegiel.net>
2017-09-19 11:45:04 +0200

    ChatBlock test

--- src/Plugin/Block/ChatBlock.php
+++ src/Plugin/Block/ChatBlock.php
@@ -4,8 +4,8 @@
  * Contains \Drupal\react_chat\Plugin\Block\ChatBlock.
  */
 
-namespace Drupal\reactjs_example\Plugin\Block;
-use Drupal\Core\Block\BlockBase;
+ namespace Drupal\react_chat\Plugin\Block; 
+ use Drupal\Core\Block\BlockBase;
 
 /**
  * Provides a 'React Live Chat Block' block.
@@ -15,16 +15,15 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("React Live Chat"),
  * )
  */
-class ChatBlock extends BlockBase {
-  /**
-   * {@inheritdoc}
-   */
-  public function build() {
-    $build = [];
-    $build['react_chat']['#markup'] = '<div id="reactchat"></div>';  
-    $build['#attached']['library'][] = 'react_chat/react.min';  
-    $build['#attached']['library'][] = 'react_chat/react.dom.min';  
-    $build['#attached']['library'][] = 'react_chat/react.chat';  
-    return $build;
-  }
-}
+ class ChatBlock extends BlockBase {
+    
+      /**
+       * {@inheritdoc}
+       */
+      public function build() {
+        return array(
+          '#markup' => $this->t('Hello, World!'),
+        );
+      }
+    
+    }
