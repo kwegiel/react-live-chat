41627a984a58b9e6901ec690101e91c18a195f5e
Krzysztof Wegiel <hello@wegiel.net>
2017-09-27 12:19:43 +0200

    Hello world

--- js/reactchat.js
+++ js/reactchat.js
@@ -70,27 +70,11 @@
 "use strict";
 
 
-function tick() {
-  var element = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Hello, world!'
-    ),
-    React.createElement(
-      'h2',
-      null,
-      'It is ',
-      new Date().toLocaleTimeString(),
-      '.'
-    )
-  );
-  ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello, world!'
+), document.getElementById('root'));
 
 /***/ })
 /******/ ]);
